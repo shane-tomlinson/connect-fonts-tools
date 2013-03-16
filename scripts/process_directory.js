cc1b5605df5b6a210ed713edaaa3d4ab4689c2f0
Shane Tomlinson <stomlinson@mozilla.com>
2013-03-16 11:49:50 +0000

    Move all processing to directory.js

--- scripts/process_directory.js
+++ scripts/process_directory.js
@@ -6,8 +6,6 @@
 
 const path          = require('path'),
       directory     = require('../lib/directory'),
-      gen_readme    = require('../lib/generate_readme'),
-      licenses      = require('../lib/licenses'),
       optimist      = require('optimist')
                           .describe('ff', 'font family')
                           .describe('an', 'Author name')
@@ -33,61 +31,11 @@ if (!(sourcePath && targetPath)) {
   process.exit(1);
 }
 
-processDirectory(sourcePath, targetPath, function(err) {
-  checkError(err);
-});
-
-
-function checkError(err) {
+directory.process(sourcePath, targetPath, argv, function(err) {
   if (err) {
     console.error(String(err));
     process.exit(1);
   }
-}
+});
 
-function generateReadme(targetPath, argv, metaInfo, done) {
-  gen_readme.write({
-    target_dir: targetPath,
-    author: {
-      name: argv.an || metaInfo.general.designer,
-      emails: argv.ae,
-      urls: argv.au || metaInfo.general.url_designer,
-      githubs: argv.ag,
-      twitter: argv.at
-    },
-    package: {
-      name: argv.pn,
-      homepage: argv.ph || false,
-      repourl: argv.pr || false,
-      bugsurl: argv.pb || false
-    },
-    font: {
-      names: argv.fn,
-      family: argv.ff || metaInfo.general.font_family
-    },
-    credits: argv.c,
-    license: argv.l ? licenses[argv.l] : {
-      version: "2.0",
-      name: metaInfo.general.license_desc,
-      url: metaInfo.general.url_license
-    }
-  }, done);
-}
-
-
-function processDirectory(sourcePath, targetPath, done) {
-  var fontTargetPath = path.join(targetPath, 'fonts', 'default');
-  directory.prepareTarget(sourcePath, fontTargetPath, function(err) {
-    if (err) return done(err);
-
-    directory.metadata(fontTargetPath, function(err, metaInfo) {
-      if (err) return done(err);
-
-      generateReadme(targetPath, argv, metaInfo, function(err) {
-        if (err) return done(err);
-
-      });
-    });
-  });
-}
 
